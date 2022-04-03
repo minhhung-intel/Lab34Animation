28d52c8b43b6a1c555b7e3ec2f4a8865b08548d4
hungnmfx00878 <minh.hung.nguyen@intel.com>
2022-04-04 05:01:41 +0700

    ini

---
+++ app/src/main/java/funix/prm/lab34_animation/MainActivity.java
@@ -0,0 +1,41 @@
+package funix.prm.lab34_animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageView ivAnimal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    private void initViews() {
+        ivAnimal = findViewById(R.id.iv_animal);
+        findViewById(R.id.bt_alpha).setOnClickListener(this);
+        findViewById(R.id.bt_rotate).setOnClickListener(this);
+        findViewById(R.id.bt_scale).setOnClickListener(this);
+        findViewById(R.id.bt_trans).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.bt_alpha) {
+            ivAnimal.startAnimation(AnimationUtils.loadAnimation(this,R.anim.alpha));
+        } else if (v.getId() == R.id.bt_rotate) {
+            ivAnimal.startAnimation(AnimationUtils.loadAnimation(this, R.anim.rotate));
+        } else if (v.getId() == R.id.bt_trans) {
+            ivAnimal.startAnimation(AnimationUtils.loadAnimation(this, R.anim.translate));
+        } else if (v.getId() == R.id.bt_scale) {
+            ivAnimal.startAnimation(AnimationUtils.loadAnimation(this, R.anim.scale));
+        }
+    }
+}
